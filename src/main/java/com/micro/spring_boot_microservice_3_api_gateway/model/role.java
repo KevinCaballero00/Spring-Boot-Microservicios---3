d82de74a311892b7c40d5b8848c073df9b225b28
KevinCaballero00 <kevinarturocc@ufps.edu.co>
2025-06-19 15:50:46 -0500

    First commit

---
+++ src/main/java/com/micro/spring_boot_microservice_3_api_gateway/model/role.java
@@ -0,0 +1,7 @@
+package com.micro.spring_boot_microservice_3_api_gateway.model;
+
+public enum role {
+    USER,
+    ADMIN
+    
+}
---
+++ src/main/java/com/micro/spring_boot_microservice_3_api_gateway/model/user.java
@@ -0,0 +1,40 @@
+package com.micro.spring_boot_microservice_3_api_gateway.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class user {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+
+    @Column(name = "username", unique = true, nullable = false, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+    
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "fecha_creacion", nullable = false)
+    private LocalDateTime fechaCreacion;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private role role;
+
+}
