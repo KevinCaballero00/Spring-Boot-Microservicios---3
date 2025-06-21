275f06fdc25c46097f4f160fc0a3d019856cd480
KevinCaballero00 <kevinarturocc@ufps.edu.co>
2025-06-21 15:50:50 -0500

    Third commit

---
+++ src/main/java/com/micro/spring_boot_microservice_3_api_gateway/security/customUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.micro.spring_boot_microservice_3_api_gateway.security;
+
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import com.micro.spring_boot_microservice_3_api_gateway.service.userService;
+import com.micro.spring_boot_microservice_3_api_gateway.utils.securityUtils;
+import com.micro.spring_boot_microservice_3_api_gateway.model.user;
+
+public class customUserDetailsService implements UserDetailsService{
+
+    @Autowired
+    private userService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+        user user = userService.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        
+        Set<GrantedAuthority> authorities = Set.of(securityUtils.convertToAuthority(user.getRole().name()));
+        
+        return userPrincipal.builder()
+                .user(user)
+                .id(user.getId())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+    
+}
